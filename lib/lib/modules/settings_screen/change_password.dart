fcbe2e894475cc647b064eea23ff8dafd6cee88a
Mohammedomargallya39 <shakalo789@gmail.com>
2022-02-18 20:26:10 +0200

    workToday

--- lib/lib/modules/settings_screen/change_password.dart
+++ lib/lib/modules/settings_screen/change_password.dart
@@ -2,10 +2,10 @@ import 'package:conditional_builder_null_safety/conditional_builder_null_safety.
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:social/lib/cubit/cubit.dart';
 import 'package:social/lib/cubit/states.dart';
 import 'package:social/lib/shared/components/components.dart';
-import 'package:social/lib/shared/styles/colors.dart';
 
 class ChangePasswordScreen extends StatelessWidget {
   var formKey = GlobalKey<FormState>();
@@ -29,35 +29,54 @@ class ChangePasswordScreen extends StatelessWidget {
             appBar: AppBar(
               title: const Text('change password'),
             ),
-            body: Form(
-              key: formKey,
-              child: SingleChildScrollView(
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(11, 22, 11, 11),
-                  child: Center(
-                    child: Column(
-                      children: <Widget>[
+            body: Container(
+              width: size.width,
+              height: size.height,
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  Positioned(
+                    top: 0,
+                    left: 0,
+                    child: Image.asset('assets/images/main_top.png',
+                      width: size.width * 0.35,),
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    right: 0,
+                    child: Image.asset('assets/images/login_bottom.png',
+                      width: size.width * 0.4,),
+                  ),
+                  Form(
+                    key: formKey,
+                    child: SingleChildScrollView(
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(11, 0, 11, 11),
+                        child: Center(
+                          child: Column(
+                            children: <Widget>[
+                              SvgPicture.asset('assets/images/forget_password.svg',height: size.height * 0.4,),
 
-                        SizedBox(height: size.height *0.038,),
-                        defaultFormField(
-                          type: TextInputType.visiblePassword,
-                          controller: passwordController,
-                          prefix: Icons.lock,
-                          text: 'Add new password',
-                          validate: (String value) {
-                            if (value.isEmpty) {
-                              return 'Password must not be empty';
-                            }
-                          },
-                          context: context,
-                        ),
 
-                        SizedBox(height: size.height *0.05,),
+                              defaultFormField(
+                                type: TextInputType.visiblePassword,
+                                controller: passwordController,
+                                prefix: Icons.lock,
+                                text: 'Add new password',
+                                validate: (String value) {
+                                  if (value.isEmpty) {
+                                    return 'Password must not be empty';
+                                  }
+                                },
+                                context: context,
+                              ),
 
-                        defaultButton(
-                          function: ()
-                          {
-                            if (formKey.currentState!.validate())
+                              SizedBox(height: size.height *0.05,),
+
+                              defaultButton(
+                                function: ()
+                                {
+                                  if (formKey.currentState!.validate())
                                   {
                                     AppCubit.get(context).updateUserData(
                                         name: nameController.text,
@@ -67,51 +86,54 @@ class ChangePasswordScreen extends StatelessWidget {
                                     showToast(message: 'password changed', state: ToastStates.SUCCESS);
                                   }
 
-                          },
-                          text: 'change password',
-                          context: context,
-                          height: size.height * 0.08,
-                          width: size.width,
-                        ),
+                                },
+                                text: 'change password',
+                                context: context,
+                                height: size.height * 0.08,
+                                width: size.width,
+                              ),
 
-                        // SizedBox(
-                        //   width: size.width ,
-                        //   height: size.height * 0.06,
-                        //   child: MaterialButton(
-                        //     shape: RoundedRectangleBorder(
-                        //       borderRadius: BorderRadius.circular(22.0),
-                        //     ),
-                        //     elevation: 5,
-                        //     color: defaultColor,
-                        //     onPressed: ()
-                        //     {
-                        //       if (formKey.currentState!.validate())
-                        //       {
-                        //         AppCubit.get(context).updateUserData(
-                        //             name: nameController.text,
-                        //             phone: phoneController.text,
-                        //             password: passwordController.text
-                        //         );
-                        //         navigateAndEnd(context, UserSettingsScreen());
-                        //         showToast(message: 'password changed', state: ToastStates.SUCCESS);
-                        //       }
-                        //
-                        //     },
-                        //     child: const Text('Change password',
-                        //       style: TextStyle(
-                        //         color: Colors.white,
-                        //         fontSize: 16,
-                        //         fontWeight: FontWeight.bold,
-                        //       ),
-                        //     ),
-                        //   ),
-                        // ),
-                      ],
+                              // SizedBox(
+                              //   width: size.width ,
+                              //   height: size.height * 0.06,
+                              //   child: MaterialButton(
+                              //     shape: RoundedRectangleBorder(
+                              //       borderRadius: BorderRadius.circular(22.0),
+                              //     ),
+                              //     elevation: 5,
+                              //     color: defaultColor,
+                              //     onPressed: ()
+                              //     {
+                              //       if (formKey.currentState!.validate())
+                              //       {
+                              //         AppCubit.get(context).updateUserData(
+                              //             name: nameController.text,
+                              //             phone: phoneController.text,
+                              //             password: passwordController.text
+                              //         );
+                              //         navigateAndEnd(context, UserSettingsScreen());
+                              //         showToast(message: 'password changed', state: ToastStates.SUCCESS);
+                              //       }
+                              //
+                              //     },
+                              //     child: const Text('Change password',
+                              //       style: TextStyle(
+                              //         color: Colors.white,
+                              //         fontSize: 16,
+                              //         fontWeight: FontWeight.bold,
+                              //       ),
+                              //     ),
+                              //   ),
+                              // ),
+                            ],
+                          ),
+                        ),
+                      ),
                     ),
                   ),
-                ),
+                ],
               ),
-            ) ,
+            )
           ),
           fallback: (context) => Center(child: CircularProgressIndicator()),
         );
--- lib/lib/modules/user_screens/user_home_screen/books/user_books_screen.dart
+++ lib/lib/modules/user_screens/user_home_screen/books/user_books_screen.dart
@@ -1,98 +1,164 @@
 import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:social/lib/cubit/cubit.dart';
 import 'package:social/lib/cubit/states.dart';
-import 'package:social/lib/models/user_books_model.dart';
 import 'package:social/lib/modules/user_screens/user_home_screen/books/user_pdf_books_screen.dart';
 import 'package:social/lib/shared/components/components.dart';
+import 'package:social/lib/shared/cubit/cubit.dart';
+import 'package:social/lib/shared/cubit/states.dart';
+import 'package:vector_math/vector_math_64.dart' as vector;
 
-class UserOnlineBooksScreen extends StatelessWidget {
-  const UserOnlineBooksScreen({Key? key}) : super(key: key);
+class UserBooksScreen extends StatefulWidget {
+  const UserBooksScreen({Key? key}) : super(key: key);
+
+  @override
+  _UserBooksScreenState createState() => _UserBooksScreenState();
+}
+
+class _UserBooksScreenState extends State<UserBooksScreen> {
+  final pageController = PageController(viewportFraction: 0.8);
+  double page = 0.0;
+  void listenScroll() {
+    setState(() {
+      page = pageController.page!;
+    });
+  }
+
+  @override
+  void initState() {
+    pageController.addListener(listenScroll);
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    pageController.removeListener(listenScroll);
+    pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-
-    return
-      BlocConsumer<AppCubit,AppStates>(
-        listener: (context, state) {},
-        builder: (context, state)
-        {
-          var cubit = AppCubit.get(context);
-          return ConditionalBuilder(
-            condition: cubit.homeModel != null,
-            builder: (context) =>  Scaffold(
-              appBar: AppBar(
-                title: const Text('Books'),
-              ),
-              body: ListView.separated(
-                  physics: const BouncingScrollPhysics(),
-                  itemBuilder: (context,index) => InkWell(child: userBooksItem(
-                      cubit.homeModel!.books![index] , context),
-                  onTap: ()
-                  {
-                    navigateTo(context, UserPDFBooksScreen(
-                      bookId : index,
-                    ));
-                  },),
-                  separatorBuilder:(context,index)=> Padding(
-                    padding: const EdgeInsets.all(8.0),
-                  ),
-                  itemCount: cubit.homeModel!.books!.length),
-            ),
-            fallback:(context) => Scaffold(
-              appBar: AppBar(title: Text('Your Books'
-              ),
-              ),
-              body: Center(
-                child: Text('You Do not have any books yet',
-                  style:
-                  TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20
-                  ),
-                ),
-              ),
-            ),
-          );
-        },
-      );
-  }
-  Widget userBooksItem(Books bookId , context) {
     Size size = MediaQuery.of(context).size;
-    return InkWell(
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(22, 22, 22, 10),
-        child: Center(
-          child: Column(
-            children: [
-               SizedBox(height: size.height * 0.02),
-              Image(
-                image: NetworkImage(
-                    '${bookId.cover}'
+    return BlocConsumer<AppCubit,AppStates>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        return BlocBuilder<ThemeCubit,ThemeStates>(
+          builder: (context, state) {
+            return ConditionalBuilder(
+              condition: AppCubit.get(context).homeModel != null,
+              builder: (context) => Scaffold(
+                floatingActionButton: IconButton(icon:Icon(Icons.arrow_back_ios)
+                  ,onPressed: ()
+                  {
+                    Navigator.pop(context);
+                  },
                 ),
-                width: size.width,
-                height: size.height * 0.22,
-              ),
-              SizedBox(height: size.height * 0.02),
-              Text(
-                '${bookId.name}',
-                style: TextStyle(
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.bold,
+                floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
+                backgroundColor: ThemeCubit.get(context).darkTheme? Colors.grey.shade900 :Colors.white,
+                body: Container(
+                  width: size.width,
+                  height: size.height,
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Positioned(
+                        top: 0,
+                        left: 0,
+                        child: Image.asset('assets/images/main_top.png',
+                          width: size.width * 0.35,),
+                      ),
+                      Positioned(
+                        bottom: 0,
+                        right: 0,
+                        child: Image.asset('assets/images/login_bottom.png',
+                          width: size.width * 0.4,),
+                      ),
+                      Center(
+                        child: SizedBox(
+                          width: size.width,
+                          height: size.height * 0.4,
+                          child: PageView.builder(
+                              controller: pageController,
+                              itemCount: AppCubit.get(context).homeModel!.books!.length,
+                              itemBuilder: (context, index) {
+                                final percent = (page - index).abs().clamp(0.0, 1.0);
+                                final factor = pageController.position.userScrollDirection ==
+                                    ScrollDirection.forward
+                                    ? 1.0
+                                    : -1.0;
+                                final opacity = percent.clamp(0.0, 0.7);
+                                return Transform(
+                                  transform: Matrix4.identity()
+                                    ..setEntry(3, 2, 0.001)
+                                    ..rotateY(vector.radians(45 * factor * percent)),
+                                  child: Opacity(
+                                    opacity: (1 - opacity),
+                                    child: Card(
+                                        color: ThemeCubit.get(context).darkTheme? Colors.grey.shade700 :Colors.grey.shade300,
+                                        child: InkWell(
+                                          child: Column(
+                                            children: [
+                                              Image(
+                                                  fit: BoxFit.cover,
+                                                  width: size.width,
+                                                  height: size.height * 0.26,
+                                                  image: NetworkImage(
+                                                      AppCubit.get(context).homeModel!.books![index].cover!
+                                                  )),
+                                              SizedBox(height: size.height * 0.02),
+                                              Text(
+                                                '${AppCubit.get(context).homeModel!.books![index].name}',
+                                                maxLines: 1,
+                                                overflow: TextOverflow.ellipsis,
+                                                style: TextStyle(
+                                                  fontSize: 16.0,
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                              SizedBox(height: size.height * 0.02),
+                                              Container(
+                                                decoration: const BoxDecoration(
+                                                  border: Border(bottom:  BorderSide(color: Colors.grey),),
+                                                ),
+                                              ),
+                                              SizedBox(height: size.height * 0.02),
+                                              Text(
+                                                '${AppCubit.get(context).homeModel!.books![index].description}',
+                                                maxLines: 1,
+                                                overflow: TextOverflow.ellipsis,
+                                                style: TextStyle(
+                                                  fontSize: 16.0,
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                          onTap: ()
+                                          {
+                                            navigateTo(context, UserPDFBooksScreen(
+                                              bookId : index,
+                                            ));
+                                          },
+                                        )
+                                    ),
+                                  ),
+                                );
+                              }),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-              SizedBox(height: size.height * 0.02),
-              Container(
-                decoration: const BoxDecoration(
-                  border: Border(bottom:  BorderSide(color: Colors.grey),),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+              fallback: (context) => Center(child: CircularProgressIndicator()),
+            );
+          },
+        );
+      },
     );
   }
 }
