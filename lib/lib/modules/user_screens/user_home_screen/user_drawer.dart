63c90062697d1e828fce650d2c7009626acbd851
Mohammedomargallya39 <shakalo789@gmail.com>
2022-03-09 19:02:58 +0200

    add about us empty screen and trying solve drawer issue

--- lib/lib/modules/user_screens/user_home_screen/user_drawer.dart
+++ lib/lib/modules/user_screens/user_home_screen/user_drawer.dart
@@ -347,6 +347,8 @@ class _HomeDrawerState extends State<HomeDrawer> {
                           ListTile(
                             title: Text(
                               'Sign Out',
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
                               style: TextStyle(
                                 fontFamily: 'Bahij Janna',
                                 fontWeight: FontWeight.w600,
@@ -450,6 +452,8 @@ class _HomeDrawerState extends State<HomeDrawer> {
                       Expanded(
                         child: Text(
                           listData.labelName,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
                           style: TextStyle(
                             fontWeight: FontWeight.w500,
                             fontSize: size.width * 0.044,
